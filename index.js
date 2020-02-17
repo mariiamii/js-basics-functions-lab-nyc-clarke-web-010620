a779b6f4e4e8091c858f0370c143894195a1c0e0
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-17 23:30:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function distanceTravelledInFeet(startBlock, endBlock) {
 function calculatesFarePrice() {
   return 
 }
+
+describe('calculatesFarePrice(start, destination)', function() {
+    it('gives customers a free sample', function() {
+      expect(calculatesFarePrice(43, 44)).to.equal(0);
+    });
+
+    it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
+      // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
+      expect(calculatesFarePrice(34, 32)).to.equal(2.56);
+    });
+
+    it('charges 25 dollars for a distance over 2000 feet', function() {
+      expect(calculatesFarePrice(50, 58)).to.equal(25);
+    });
+
+    it('does not allow rides over 2500 feet', function() {
+      expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
+    });
+  });
