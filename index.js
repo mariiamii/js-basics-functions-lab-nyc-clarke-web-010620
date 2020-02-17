6d89a632f1b5eb35c948087d58cd01746e4d6cba
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-17 23:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,28 +10,17 @@ function distanceTravelledInFeet(startBlock, endBlock) {
   return Math.abs(endBlock - startBlock) * 264;
 }
 
-function calculatesFarePrice() {
-  switch (distance) {
-    case <= 400
+function calculatesFarePrice(startBlock, endBlock) {
+  let distance = distanceTravelledInFeet(startBlock, endBlock)
+  
+  switch () {
+    case distance <= 400:
+      
+    case distance > 400 && <= 2000:
+      
+    case distance > 2000 && 2500:
+      
+    case distance >= 2500:
     
   }
 }
-
-describe('calculatesFarePrice(start, destination)', function() {
-    it('gives customers a free sample', function() {
-      expect(calculatesFarePrice(43, 44)).to.equal(0);
-    });
-
-    it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
-      // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
-      expect(calculatesFarePrice(34, 32)).to.equal(2.56);
-    });
-
-    it('charges 25 dollars for a distance over 2000 feet', function() {
-      expect(calculatesFarePrice(50, 58)).to.equal(25);
-    });
-
-    it('does not allow rides over 2500 feet', function() {
-      expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
-    });
-  });
