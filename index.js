e6ef25eb6c60ffc48d52d3e3f25bca5df37bbf91
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-17 23:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,17 +13,3 @@ function distanceTravelledInFeet(startBlock, endBlock) {
 function calculatesFarePrice() {
   return 
 }
-
-describe('distanceTravelledInFeet()', function() {
-    it('returns the distance travelled in feet', function() {
-      expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
-    });
-
-    it('returns a distance in feet', function() {
-      expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
-    });
-
-    it('returns distance when destination is below distance', function() {
-      expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
-    });
-  });
