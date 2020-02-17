a49dff2224c9ade4d456cb774ed022381726566c
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-17 23:20:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet() {
+  return
+}
+
+function calculatesFarePrice() {
+  return 
+}
+
+describe('distanceFromHqInFeet()', function() {
+    it('returns a distance in feet', function() {
+      expect(distanceFromHqInFeet(43)).to.equal(264);
+    });
+
+    it('returns a distance in feet', function() {
+      expect(distanceFromHqInFeet(50)).to.equal(2112);
+    });
+
+    it('calculates distances below 42nd street', function() {
+      expect(distanceFromHqInFeet(34)).to.equal(2112);
+    });
+  });
